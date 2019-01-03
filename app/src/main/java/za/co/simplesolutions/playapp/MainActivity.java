df0971c55e00180805321c575a8d8a3a21dfd1b8
Moses Mosese <mosesm.mosese@gmail.com>
2019-01-03 20:25:04 +0200

    fundamentals tutorial 1

--- app/src/main/java/za/co/simplesolutions/playapp/MainActivity.java
+++ app/src/main/java/za/co/simplesolutions/playapp/MainActivity.java
@@ -3,23 +3,47 @@ package za.co.simplesolutions.playapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "za.co.simplesolutions.MESSAGE";
+    private int mCount = 0;
+    private TextView mShowCount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mShowCount = findViewById(R.id.show_count);
+
+        Log.i(MainActivity.class.getName(), "started...");
     }
 
-    public void sendMessage(View view) {
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        ++mCount;
+        if(mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
+    }
+
+    /**public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = findViewById(R.id.editText);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
-    }
+    }**/
 }
