10d0e0102c2db3de0df1fd9c9d13f313d1c0e78f
Moses Mosese <mosesm.mosese@gmail.com>
2019-01-20 17:42:45 +0200

    3.3: Support libraries

--- app/src/main/java/za/co/simplesolutions/playapp/MainActivity.java
+++ app/src/main/java/za/co/simplesolutions/playapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ShareCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -11,63 +12,45 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     public static final String EXTRA_MESSAGE = "za.co.simplesolutions.extra.MESSAGE";
-    private EditText websiteEditTxt;
-    private EditText locationEditTxt;
-    private EditText shareTextEditTxt;
+    private TextView helloTxtVw;
+    private String[] mColorArray = {"red", "pink", "purple", "deep_purple",
+            "indigo", "blue", "light_blue", "cyan", "teal", "green",
+            "light_green", "lime", "yellow", "amber", "orange", "deep_orange",
+            "brown", "grey", "blue_grey", "black" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        websiteEditTxt = findViewById(R.id.web_edittext);
-        locationEditTxt = findViewById(R.id.location_edittext);
-        shareTextEditTxt = findViewById(R.id.share_edittext);
+        helloTxtVw = findViewById(R.id.hello_textview);
+
+        if(savedInstanceState != null) {
+            helloTxtVw.setTextColor(savedInstanceState.getInt("color"));
+        }
 
         Log.i(MainActivity.class.getName(), "started...");
     }
 
-
-    public void openWebsite(View view) {
-        Log.d(LOG_TAG, "opening website");
-
-        String url = websiteEditTxt.getText().toString();
-        Uri webpage = Uri.parse(url);
-        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
-        if(intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        }else {
-            Log.d(LOG_TAG, "Cant handle this!");
-        }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("color", helloTxtVw.getCurrentTextColor());
     }
 
-    public void openLocation(View view) {
-        Log.d(LOG_TAG, "opening location");
-        String loc = locationEditTxt.getText().toString();
-        Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
-        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
-        if(intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        }
-        else {
-            Log.d(LOG_TAG, "Cant handle this intent!");
-        }
-    }
+    public void changeColor(View view) {
+        Random random = new Random();
+        String colorName = mColorArray[random.nextInt(mColorArray.length)];
+        int colorResourceName = getResources().getIdentifier(colorName, "color", getApplicationContext().getPackageName());
+        int colorRes = ContextCompat.getColor(this, colorResourceName);
+        helloTxtVw.setTextColor(colorRes);
 
-    public void shareText(View view) {
-        Log.d(LOG_TAG, "sharing text");
 
-        String text = shareTextEditTxt.getText().toString();
-        String mimeTYpe = "text/plain";
-        ShareCompat.IntentBuilder
-                .from(this)
-                .setType(mimeTYpe)
-                .setChooserTitle(R.string.txt_chooser_title)
-                .setText(text)
-                .startChooser();
     }
 }
